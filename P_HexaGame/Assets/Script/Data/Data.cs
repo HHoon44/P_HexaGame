f8cf1c9d43aeff632ba31d44e6eed17de69fa73d
MeLeeTa <sonmet99@metaskyworld.net>
2023-09-21 16:31:12 +0900

    점검

---
+++ P_HexaGame/Assets/Script/Data/Data.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+/// <summary>
+/// 데이터를 지니고 있는 클래스
+/// </summary>
+public static class Data
+{
+    public enum TileColor
+    {
+        None, Blue, Cyan, Green, Orange, Purple, Red, Yellow
+    }
+
+    sds
+}
---
+++ P_HexaGame/Assets/Script/Object/Board.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spawnPos;
+    //private Vector2Int spawnPos = new Vector2Int(-1, 8);
+
+}
